b240171ed09520d6c14eab85134bd6515d15ed73
96nic <96nico1@gmail.com>
2020-05-13 22:42:27 +0200

    build bugfix

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -509,19 +509,6 @@ module.exports = function (webpackEnv) {
       ],
     },
     plugins: [
-      new CircularDependencyPlugin({
-        // exclude detection of files based on a RegExp
-        exclude: /a\.js|node_modules/,
-        // include specific files based on a RegExp
-        include: /src/,
-        // add errors to webpack instead of warnings
-        failOnError: false,
-        // allow import cycles that include an asyncronous import,
-        // e.g. via import(/* webpackMode: "weak" */ './file.js')
-        allowAsyncCycles: false,
-        // set the current working directory for displaying module paths
-        cwd: process.cwd(),
-      }),
       // Generates an `index.html` file with the <script> injected.
       new HtmlWebpackPlugin(
         Object.assign(
@@ -535,11 +522,11 @@ module.exports = function (webpackEnv) {
                 minify: {
                   removeComments: true,
                   collapseWhitespace: true,
-                  removeRedundantAttributes: true,
-                  useShortDoctype: true,
-                  removeEmptyAttributes: true,
-                  removeStyleLinkTypeAttributes: true,
-                  keepClosingSlash: true,
+                  removeRedundantAttributes: false,
+                  useShortDoctype: false,
+                  removeEmptyAttributes: false,
+                  removeStyleLinkTypeAttributes: false,
+                  keepClosingSlash: false,
                   minifyJS: true,
                   minifyCSS: true,
                   minifyURLs: true,
